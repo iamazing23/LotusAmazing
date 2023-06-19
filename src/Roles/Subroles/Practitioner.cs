685c5415fcdac6c11dc41fc958a13177b309f24d
iamazing23 <98273920+iamazing23@users.noreply.github.com>
2023-06-18 19:59:01 -0700

    Role: Practitioner
    Can see dead peoples cause of death

--- src/Roles/Subroles/Practitioner.cs
+++ src/Roles/Subroles/Practitioner.cs
@@ -4,27 +4,20 @@ using Lotus.GUI.Name.Components;
 using Lotus.GUI.Name.Holders;
 using Lotus.Managers.History.Events;
 using Lotus.Roles.Internals.Attributes;
-using Lotus.Roles.RoleGroups.Vanilla;
 using Lotus.Roles.Subroles;
 using UnityEngine;
 using VentLib.Localization.Attributes;
-using VentLib.Options.Game;
 using VentLib.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Lotus.Factions;
-using Lotus.Factions.Interfaces;
-using Lotus.Extensions;
-using Lotus.Roles.Extra;
-using Lotus.Roles.Interfaces;
+
 
 namespace Lotus.Roles.RoleGroups.Crew;
 
 [Localized("Roles.Subroles.Practitioner")]
 
-public class Practitioner : Crewmate
+public class Practitioner : Subrole
 {
+    public override string Identifier() => "♬";
+
     [RoleAction(RoleActionType.AnyDeath)]
     private void PractitionerAnyDeath(PlayerControl dead, IDeathEvent causeOfDeath)
     {
@@ -37,8 +30,8 @@ public class Practitioner : Crewmate
 
     [Localized(nameof(PractitionerDescription))]
     public static string PractitionerDescription = "The Practitioner lets you see how people died.";
-    protected override RoleModifier Modify(RoleModifier roleModifier) =>
-        base.Modify(roleModifier).RoleColor(new Color(0.32f, 0.74f, 1f));
-    public static class Options { }
-
+    protected override RoleModifier Modify(RoleModifier roleModifier)
+    {
+        return base.Modify(roleModifier).RoleColor(new Color(0.32f, 0.74f, 1f));
+    }
 }
